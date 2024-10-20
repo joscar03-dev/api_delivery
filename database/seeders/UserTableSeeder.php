4cfb27bf2876ff5d5081dc8dfdb7640f34350f23
joscar03-dev <136681347+joscar03-dev@users.noreply.github.com>
2024-10-20 17:15:24 -0500

    seeder para user

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table(
+                'users'
+            )->insert([
+
+                'name'
+                =>
+                'John Doe',
+
+                'email'
+                =>
+                'johndoe@example.com',
+
+                'password'
+                =>
+                Hash
+                    ::make(
+                        'password'
+                    ), // Encripta la contraseña
+
+                'created_at'
+                =>
+                now(),
+
+                'updated_at'
+                =>
+                now(),
+            ]);
+    }
+}
