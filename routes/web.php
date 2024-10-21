0ed8bb0d9b53778bcb3ca13f749afbec39dfc647
joscar03-dev <locomancocapac@gmail.com>
2024-10-21 17:28:57 -0500

    cambio de ruta a login

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 /* Route::get('/', function () {
     return view('welcome');
-}); */
-Route::get('/', function () {
-    return view('/admin');
 });
-
+ */
+Route::get('/',function () {
+    return redirect('/admin');
+    }
+);
