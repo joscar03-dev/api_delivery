7a2c88c3efae2ffdfe0f4f6d113937ad0292ba53
joscar03-dev <locomancocapac@gmail.com>
2024-10-21 15:45:20 -0500

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/* Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
 });
- */
-Route::get('/',function () {
-    return redirect('/admin');
-    }
-);
